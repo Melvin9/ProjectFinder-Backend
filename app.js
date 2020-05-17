c6733dba679bdc9085e0ca30a2361b377a59a04b
Melvin9 <melvinmathew0102@gmail.com>
2020-05-17 22:29:39 +0530

    Route Updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var physicsRouter = require("./routes/physics");
+var physicsRouter = require("./routes/projects");
 
 var app = express();
 mongoose.connect(
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/physics", physicsRouter);
+app.use("/projects", physicsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
